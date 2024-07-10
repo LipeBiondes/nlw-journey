e4af1bb02572a686565a7cc87eb81f76df241342
Lipe biondes <alefefelipebiondesmagalhaes@gmail.com>
2024-07-10 19:18:51 -0300

    feat: Handle client errors in errorHandle function

--- backend/src/error-handle.ts
+++ backend/src/error-handle.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance } from 'fastify'
+import { ClientError } from './errors/client-error'
 
 type FastifyErrorHandle = FastifyInstance['errorHandler']
 
@@ -7,6 +8,10 @@ export const errorHandle: FastifyErrorHandle = async (
   request,
   reply
 ) => {
+  if (error instanceof ClientError) {
+    return reply.status(400).send({ message: error.message })
+  }
+
   console.log(error)
   reply.status(500).send({ message: 'Internal server error' })
 }
