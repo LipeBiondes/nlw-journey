02610f66bafcb88d0ef4a48546d3942e598ea62a
Lipe biondes <alefefelipebiondesmagalhaes@gmail.com>
2024-07-12 12:27:18 -0300

    feat: Add dark mode support to input fields

--- frontend/src/pages/create-trip/steps/invite-guests-step.tsx
+++ frontend/src/pages/create-trip/steps/invite-guests-step.tsx
@@ -1,4 +1,5 @@
 import { ArrowRight, UserRoundPlus } from 'lucide-react'
+import { Button } from '../../components/button'
 
 interface InviteGuestsStepProps {
   openGuestsModal: () => void
@@ -28,13 +29,10 @@ export function InviteGuestsStep({
         )}
       </button>
 
-      <button
-        onClick={openConfirmTripModal}
-        className="bg-lime-300 text-lime-900 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
-      >
+      <Button variant="primary" size="default" onClick={openConfirmTripModal}>
         Confirmar viagem
         <ArrowRight className="size-5" />
-      </button>
+      </Button>
     </div>
   )
 }
